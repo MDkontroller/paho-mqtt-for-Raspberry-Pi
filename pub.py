1d22a6172558c5282fa377f34a77ced535371d80
MDkontroller <43541921+MDkontroller@users.noreply.github.com>
2021-12-10 16:13:52 +0100

    last updated files

---
+++ pub.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as pb
+from time import sleep
+
+MQTT_SERVER = "127.0.0.1" #169.254.71.208" local network()
+MQTT_PATH   = "channel_1" # topic to be publish /subscribed
+
+temp = 0.0
+cycle = 1
+t = 0.0
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(MQTT_PATH)
+    print("Connected.  result:" +str(rc))
+
+def on_message(client,userdata,msg):
+	print("topic:"+ msg.topic + "  distance: " +str(msg.payload))
+	#temp = float(msg.payload)
+	temp = float(msg.payload)
+	
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+#client.connect(MQTT_SERVER, 1883, 60)
+client.connect(MQTT_SERVER)
+client.loop_start()
+
+while True:
+    
+    sleep(0.5)
+    temp += 1   
+    pb.single(MQTT_PATH, temp, hostname = MQTT_SERVER)
---
+++ sub.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+
+MQTT_SERVER = "127.0.0.1" # other raspi IP adress
+MQTT_PATH   = "channel_1" # topic to subscriber
+
+temp = 0.0
+cycle = 0
+
+def on_connect (client, userdata, flags, rc):
+    client.subscribe(MQTT_PATH)
+    #print("Conenected to TOPIC")
+
+def on_message(client, userdata, msg):
+    #print("Topic " + msg.topic + " message: " + str (msg.payload)) # for visualization
+    global temp
+    #global cycle 
+    temp = float(msg.payload)
+   
+    
+def connection():
+    
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(MQTT_SERVER, 1883, 60)
+    client.loop_start()
+    client.disconnect()
+
+while True:
+    
+    sleep(0.5)
+    connection()
+    print(temp)
